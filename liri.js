6e60d7cfe7b020f076eb2309191642fa1fb4dbd6
StephenDK <stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.com>
2018-01-21 15:02:22 -0800

    OMDB search finalized

--- liri.js
+++ liri.js
@@ -49,11 +49,46 @@ function operatorConditionChecker () {
 };
 
 function movieSearch (userInput) {
+	// conditional if the user input is undefined
+	if (userInput === undefined) {
+		userInput = "Mr Nobody";
+	}
+
+
 	// var for holding the omdb movie url  
 	var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
-}
+	// Tests and debugging
+	console.log(queryUrl);
+
+	// node request for retreiving data
+	request(queryUrl, function(error, response, body){
+		// conditional incase of error
+		if (!error && response.statusCode === 200) {
+			var jsonData = JSON.parse(body);
+
+			var data = {
+				"Title": jsonData.Title,
+				"Year" : jsonData.Year,
+				"Rated": jsonData.Rated,
+				"IMDB Rating": jsonData.imdbRating,
+				"Country": jsonData.Country,
+				"Language": jsonData.Language,
+				"Plot": jsonData.Plot,
+				"Actors": jsonData.Actors,
+				"Rotten Tomatoes Rating": jsonData.Ratings[1].Value
+			};
+			console.log("--------------------")
+			console.log(data)
+
+		}
+	});
+};
 
 // Section 3:
 // Main Process
 // ===========================================================
+// Function to check operator input
 operatorConditionChecker();
+
+// Perform movie search function and pass in userInput as argument
+movieSearch(userInput);
