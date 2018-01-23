f34f04c7faca421035438600f2c43609810d2523
StephenDK <stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.com>
2018-01-23 13:07:19 -0800

    Added spotify api Keys

--- keys.js
+++ keys.js
@@ -11,7 +11,15 @@ var twitterKeys = {
 	access_token_key: '896849559107678209-RqxnGtoM00nIHLkYMgkKTc6LPNW7YOu',
 	access_token_secret: 'guYVvT2IZxEYAVpRadprW0324aDjWRamGx4rX8NBhtry8',
 };
+// 
+// Spotify api client keys
+var spotifyKeys = {
+	id: 'c625fe362cc341ae9e45cf44e0dd4778',
+	secret: 'b5ae85e2fd11404e9663de1460a31435',
+}
 
 // Add twitterKeys data to the module.exports object
 // and access them from the app.js file.
 module.exports = twitterKeys;
+// Add  spotify keys to export
+module.exports = spotifyKeys;
